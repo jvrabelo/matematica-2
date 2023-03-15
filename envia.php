0a002b893c2999747e85739d6eeb729b299c5d5e
jvrabelo <106701350+jvrabelo@users.noreply.github.com>
2023-03-15 14:35:21 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -7,7 +7,7 @@
 $from = 'rei.geo@hotmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendo = 'oliveira.reinaldo@escola.pr.gov.br';
+$sendo = 'joao.souza.rabelo@escola.pr.gov.br';
 
 // subject of the email
 $subject = 'Forumlário do site';
